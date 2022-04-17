a427267edff59a9fde853b28c8e02337abfbc087
MKesenheimer <mkesenheimer@gmx.net>
2022-04-17 23:01:40 +0200

    Formatting

--- FuelBar.cpp
+++ FuelBar.cpp
@@ -2,6 +2,6 @@
 
 FuelBar::FuelBar(float x, float y, float hsize, float vsize) 
     : Object(x, y, 0, 0, hsize, vsize, 0, 0) {
-    newPoint(0,  0, 0, 255, 255);
-    newPoint(1,  0, 0, 255, 255);
+    newPoint(0, 0, 0, 255, 255);
+    newPoint(1, 0, 0, 255, 255);
 }
